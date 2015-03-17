c7873ab90920ce060307925e468bbaed595447b4
Zozingzeus <Zozingzeus@gmail.com>
2015-03-17 23:38:50 +0100

    init

---
+++ Assets/InControl/Source/Custom Profiles/Keyboard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using InControl;
+
+
+namespace CustomProfile
+{
+	// This custom profile is enabled by adding it to the Custom Profiles list
+	// on the InControlManager component, or you can attach it yourself like so:
+	// InputManager.AttachDevice( new UnityInputDevice( "KeyboardAndMouseProfile" ) );
+	// 
+	public class Keyboard : UnityInputDeviceProfile
+	{
+		public Keyboard()
+		{
+			Name = "Keyboard/Mouse";
+			Meta = "A keyboard and mouse combination profile appropriate for FPS.";
+			
+			// This profile only works on desktops.
+			SupportedPlatforms = new[]
+			{
+				"Windows",
+				"Mac",
+				"Linux"
+			};
+			
+			Sensitivity = 1.0f;
+			LowerDeadZone = 0.0f;
+			UpperDeadZone = 1.0f;
+			
+			ButtonMappings = new[]
+			{
+				new InputControlMapping
+				{
+					Handle = "Fire - Mouse",
+					Target = InputControlType.RightTrigger,
+					Source = MouseButton0
+				},
+				new InputControlMapping
+				{
+					Handle = "AltFire",
+					Target = InputControlType.LeftTrigger,
+					Source = MouseButton2
+				},
+				new InputControlMapping
+				{
+					Handle = "Jump",
+					Target = InputControlType.Action1,
+					Source = KeyCodeButton( KeyCode.Space )
+				},
+			};
+			
+			AnalogMappings = new[]
+			{
+				new InputControlMapping
+				{
+					Handle = "Move X",
+					Target = InputControlType.LeftStickX,
+					// KeyCodeAxis splits the two KeyCodes over an axis. The first is negative, the second positive.
+					Source = KeyCodeAxis( KeyCode.A, KeyCode.D )
+				},
+				new InputControlMapping
+				{
+					Handle = "Move Y",
+					Target = InputControlType.LeftStickY,
+					// Notes that up is positive in Unity, therefore the order of KeyCodes is down, up.
+					Source = KeyCodeAxis( KeyCode.S, KeyCode.W )
+				},
+				new InputControlMapping {
+					Handle = "Move X Alternate",
+					Target = InputControlType.LeftStickX,
+					Source = KeyCodeAxis( KeyCode.LeftArrow, KeyCode.RightArrow )
+				},
+				new InputControlMapping {
+					Handle = "Move Y Alternate",
+					Target = InputControlType.LeftStickY,
+					Source = KeyCodeAxis( KeyCode.DownArrow, KeyCode.UpArrow )
+				}
+			};
+		}
+	}
+}
+
